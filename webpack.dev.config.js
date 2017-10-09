440cd4bc722cd4b2fb4d69ae66df83bae104ebe0
DESKTOP-9QDTJ1B\hp <gaurav2393@gmail.com>
2017-10-09 07:34:44 +0530

    no message

---
+++ webpack.dev.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+    devtool: "inline-source-map",
+    entry: [
+        'babel-polyfill',
+        'webpack-hot-middleware/client?reload=true',
+        path.resolve(__dirname, "./src/js/main.jsx")
+    ],
+    output: {
+    	path: path.resolve(__dirname, "./dist"),
+    	filename: "bundle.js",
+    	publicPath: "/"
+    },
+    resolve: {
+    	extensions: ['.js', '.jsx']
+    },
+    devServer: {
+
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+     module: {
+        loaders: [
+            {
+                enforce : 'pre',
+                test : /\.js$/,
+                loader : 'eslint-loader',
+                exclude : /(node_modules)/
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            }, {
+                test: /\.css$/,
+                loaders: [{
+                    loader: "style-loader"
+                },
+                    {
+                        loader: "css-loader"
+                    }]
+            }
+        ]
+    }
+}
