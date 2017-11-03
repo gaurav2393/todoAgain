5f0ae2227065b204754ec511999f9b615df5a7b1
Gaurav Gera (GAUG) <gaug@saxobank.com>
2017-11-03 10:16:19 +0530

    no message

---
+++ src/js/modules/header/container.js
@@ -0,0 +1,17 @@
+//connect ke liye
+import { connect } from 'react-redux';
+import Error from './error';
+import * as errorActions from './actions';
+
+const mapStateToProps = (store) => {
+    return {
+        showError: store.error.showError,
+        errMessage: store.error.errMessage,
+    };
+};
+
+const matDispatchToProps = {
+    hideError: errorActions.hideError,
+};
+
+export default connect(mapStateToProps, matDispatchToProps)(Error);
