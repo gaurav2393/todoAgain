41cad2e2e7b3a80c769c65f623a90c04d2173183
Gaurav Gera (GAUG) <gaug@saxobank.com>
2018-01-17 18:33:55 +0530

    no message

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SecondaryNav from "./modules/secondaryCustomNavigation";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+import Blog from './modules/blog';
+import ErrorComp from './modules/error';
 
 class Main extends React.Component {
     render(){
         return(
-            <SecondaryNav/>
+            <Router>
+                <div>
+                    <div>
+                        <Switch>
+                            <Route exact path='/' component={SecondaryNav} />
+                            <Route path='/blogs' component={Blog} />
+                            <Route path='/*' component={ErrorComp} />
+                        </Switch>
+                    </div>
+                    <div>
+                        <Link to='/'>Home</Link>
+                        <Link to='/blogs'>Blogs</Link>
+                        <Link to='/whatever/geg'>anything Else</Link>
+                    </div>  
+                </div>
+            </Router>            
         );
     }
 }
---
+++ src/js/modules/blog/blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Blog extends React.PureComponent {
+    render() {
+        return(
+            <div>
+                <p>Hi from Blog</p>
+            </div>
+        )
+    }
+}
+
+export default Blog;
