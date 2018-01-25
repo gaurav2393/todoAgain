38ac410a69d440464eff2bbd05ab07eb1c0b15d1
Gaurav Gera (GAUG) <gaug@saxobank.com>
2018-01-25 16:57:02 +0530

    asdsd

--- src/js/modules/blogs/likeAndShare.js
+++ src/js/modules/blogs/likeAndShare.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 export default function LikeAndShare() {
     return (
-        <div class="like-share">
+        <div className="like-share">
             <ul>
                 <li>
-                    <a class="like-blog" title="Like it">
+                    <a className="like-blog" title="Like it">
                         <img src="images/svgs/like.svg" alt="Like Image" />
                     </a>
                 </li>
                 <li>
-                    <a class="share-blog" title="Share the blog">
+                    <a className="share-blog" title="Share the blog">
                         <i></i>
                     </a>
                 </li>
                 <li>
-                    <a class="more-options-blog" title="More Options">
+                    <a className="comment-on-blog" title="Have some question, Leave a comment">
+                        <i></i>
+                    </a>
+                </li>
+                <li>
+                    <a className="more-options-blog" title="More Options">
                         <i></i>
                     </a>
                 </li>
