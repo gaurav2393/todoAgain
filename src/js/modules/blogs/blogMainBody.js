b43e3800b6de9c859cbb5a10b3e8e617918638fd
Gaurav Gera (GAUG) <gaug@saxobank.com>
2018-01-29 20:08:20 +0530

    no message

--- src/js/modules/blogs/blogMainBody.js
+++ src/js/modules/blogs/blogMainBody.js
@@ -3,9 +3,16 @@ import React from 'react';
 class BlogMainBody extends React.PureComponent {
     render(){
         return (
-            <div>
-                
+            <div class="blog-main-body">
+                <div class="blog-main-image">
+                    <img src="images/blogTestImage.png" />
+                </div>
+                <div>
+
+                </div>
             </div>
         )
     }
 }
+
+export default BlogMainBody;
