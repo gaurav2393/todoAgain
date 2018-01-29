3cc2dddd3560094ff22b95105898b46afdb3dc56
Gaurav Gera (GAUG) <gaug@saxobank.com>
2018-01-29 19:55:42 +0530

    no message

--- src/js/modules/blogs/blogs.js
+++ src/js/modules/blogs/blogs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AuthorBasicInfo from './authorBasicInfo';
 import LikeAndShare from './likeAndShare';
+import BlogMainBody from './blogMainBody';
 
 class Blogs extends React.PureComponent {
     render() {
@@ -10,6 +11,9 @@ class Blogs extends React.PureComponent {
                 <AuthorBasicInfo name='Gaurav Gera' imagePath='images/testImage.png' date='18 Jan' 
                     shortDesc='Description about the author' timeToRead='7 min' 
                 />
+                {/*Either the content data or else send the id number so that compnent can make the api hit for the data*/}
+                <BlogMainBody blogid="idNumber" />
+                
             </div>
         )
     }
