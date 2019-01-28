0184a8d9b6c570b91f803bbcea5fb7ef53d3cec5
gaurav2393 <gaurav2393@gmail.com>
2019-01-28 12:09:49 +0530

    no message

--- server/api/courses/coursesController.js
+++ server/api/courses/coursesController.js
@@ -1,13 +1,14 @@
 var Course = require('./coursesModel');
 var _ = require('lodash');
+const path = require('path');
 
 exports.params = function(req, res, next, id) {
 
 }
 
 exports.getTodoCourses = function(req, res, next) {
-    res.sendFile(path.join(__dirname, '../data/data.json'));
+    res.sendFile(path.join(__dirname, '../../../data/coursesTodo.json'));
 }
 exports.postTodoCourses = function(req, res, next) {
-    res.sendFile(path.join(__dirname, '../data/data.json'));
+    res.sendFile(path.join(__dirname, '../../../data/coursesTodo.json'));
 }
