fada694d58a20cb3c4607d3f851ee58635b6d4d1
DESKTOP-9QDTJ1B\hp <gaurav2393@gmail.com>
2019-01-30 07:27:05 +0530

    no message

--- server/api/questions/questionsController.js
+++ server/api/questions/questionsController.js
@@ -21,20 +21,40 @@ exports.params = function(req, res, next, id) {
 }
 
 exports.getTodoQuestions = function(req, res, next) {
-    res.sendFile(path.join(__dirname, '../../../data/coursesTodo.json'));
-    // res.sendFile(path.join(__dirname, '../data/data.json'));
+    // res.sendFile(path.join(__dirname, '../../../data/coursesTodo.json'));
+    Question.find({}, function(err, data){
+        console.log(">>>> " + data );
+    });
+    res.sendFile(path.join(__dirname, '../data/data.json'));
 }
 exports.postTodoQuestions = function(req, res, next) {
     console.log('received file', req.body[0]);
     var length = req.body.length;
     var count;
     var data = [];
-    Question.countDocuments({}, function(err, data){
-        count = data;
-    })
+    // var count = Question.where({}).countDocuments();
+    count = 0;
+    // query.countDocuments({}, function(err, data) {
+    //     count = data;
+    // });
     for (let i = 0 ; i < length ; i++) {
         let temp = {};
-        temp['']
+        temp['questionId'] = count;
+        count++;
+        temp['questionName'] = req.body[i]['A'];
+        temp['answerOptions'] = [];
+        let splitQuestionOptions = req.body[i]['B'].split(',');
+        let optionsCount = splitQuestionOptions.length;
+        for(let j = 0 ; j < optionsCount ; j++) {
+            temp['answerOptions'].push(splitQuestionOptions[j]);
+        }
+        temp['questionMainTopic'] = req.body[i]['C'];
+        temp['questionSubTopic'] = req.body[i]['D'];
+        temp['answer'] = req.body[i]['E'];
+        data.push(temp);
     }
+    Question.insertMany(data, { ordered: false }, function(err, docs) {
+        console.log('pushed', docs);
+    })
     res.sendFile(path.join(__dirname, '../../../data/coursesTodo.json'));
 }
